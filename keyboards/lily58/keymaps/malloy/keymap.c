01d718712aad3fdd3ded3a720791b69a55c55dea
Jacob Malloy <sjakem@att.net>
2026-05-01 16:56:21 -0400

    update my gaming layout a little

--- keyboards/lily58/keymaps/malloy/keymap.c
+++ keyboards/lily58/keymaps/malloy/keymap.c
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LALT, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                     KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,  XXXXXXX, KC_ENT,  KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-                   KC_LCTL, KC_SPC, XXXXXXX, KC_LSFT, KC_SPC, MO(_ADJUST), KC_RCTL, XXXXXXX
+                   KC_LCTL, XXXXXXX, KC_SPC, KC_LSFT, KC_SPC, MO(_ADJUST), KC_RCTL, XXXXXXX
   )
 };
 
